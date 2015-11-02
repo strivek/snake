462bf02ccdf60fc3943eb0c1349ecbaa92449151
GaoFei <strivek@163.com>
2015-11-02 12:59:21 +0800

    增加react-demo

---
+++ react-demos/demo01/js/components/reactApp.react.jsx
@@ -0,0 +1,14 @@
+
+var React = require('../assets/react');
+
+/**
+ * @desc export 必须有，ReactApp首字母需要大写
+ */
+
+
+export var ReactApp = React.createClass({
+    render:function(){
+        return (<div className="container" >list</div>);
+    }
+});
+
