bcb2e6c05d668dea404b8c4982dd793c9eb3d870
GaoFei <strivek@163.com>
2015-11-02 14:42:26 +0800

    增加demo2和demo3

---
+++ react-demos/demo02/js/components/demoList.react.jsx
@@ -0,0 +1,15 @@
+var React = require('../assets/react');
+
+export var DemoList = React.createClass({
+    render:function(){
+        return(
+            <ul className="list">
+                <li>list</li>
+                <li>list</li>
+                <li>list</li>
+                <li>list</li>
+                <li>list</li>
+            </ul>
+        )
+    }
+});
