e682cba9ece1b2baa7995d76736d6591ccd4d51b
GaoFei <strivek@163.com>
2015-11-02 16:15:19 +0800

    增加demo03实例
    利用，props和state进行数值传递

--- react-demos/demo03/js/components/demoList.react.jsx
+++ react-demos/demo03/js/components/demoList.react.jsx
@@ -1,14 +1,31 @@
 var React = require('../assets/react');
 
+import {DemoItem} from 'demoItem.react';
+
 export var DemoList = React.createClass({
-    render:function(){
-        return(
+    getDefaultProps: function () {
+        return {
+            list: ['张三', '李四', '王五']
+        }
+    },
+    getInitialState: function () {
+        return null;
+    },
+    //将要进行render，虚拟dom与真实dom做比较
+    componentWillMount: function () {
+    },
+    //结束render
+    componentDidMount: function () {
+        //loading结束
+    },
+    render: function () {
+        return (
             <ul className="list">
-                <li>list</li>
-                <li>list</li>
-                <li>list</li>
-                <li>list</li>
-                <li>list</li>
+                {this.props.list.map(function (value, key) {
+                    return (
+                        <DemoItem key={key} name={value}/>
+                    )
+                })}
             </ul>
         )
     }
--- react-demos/demo03/js/components/reactApp.react.jsx
+++ react-demos/demo03/js/components/reactApp.react.jsx
@@ -8,9 +8,14 @@ var React = require('../assets/react');
 import {DemoList} from 'demoList.react';
 
 export var ReactApp = React.createClass({
+    getInitialState:function(){
+      return{
+          list:['andy','job','eve']
+      }
+    },
     render:function(){
         return (<div className="container" >
-            <DemoList />
+            <DemoList  list={this.state.list} />
         </div>);
     }
 });
